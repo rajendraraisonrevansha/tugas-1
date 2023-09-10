bef66f9e75c8ae2cc37654008c9928132df9b77d
rajendraraisonrevansha <121307789+rajendraraisonrevansha@users.noreply.github.com>
2023-09-10 23:45:00 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,14 +2,17 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
-
+  
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year - this.tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan nim $nim dan usia $usia tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021081", "Rajendra Zaki Revansha", 2003);
+  
+  mhs.perkenalan();
 }
